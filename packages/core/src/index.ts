e6515e836c15e7db75a605926013dd7805066289
hottredpen <277488199@qq.com>
2025-09-26 12:23:05 +0800

    1.0.15

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,7 +5,13 @@ export * from './EC/BaseModelComp';
 export * from './EC/Comp';
 export * from './EC/Entity';
 export * from './EC/System';
-// Manager
+// Managers
+// table
+export * from './Table/Table';
 export * from './Table/TableManager';
+// event
 export * from './Event/EventManager';
+// 
+export * from './Factory/Factory';
 export * from './Factory/FactoryManager';
+export * from './Factory/Item';
