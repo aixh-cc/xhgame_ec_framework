821c6063f90216dd0ac45cb06ef6487c9094e2a6
hottredpen <277488199@qq.com>
2025-10-12 14:07:58 +0800

    新增3dDemo及xhgame_plugin

--- packages/core/src/Audio/AudioManager.ts
+++ packages/core/src/Audio/AudioManager.ts
@@ -1,11 +1,38 @@
 import { IAudioDrive } from "./AudioDrive"
 
-export class AudioManager<T extends IAudioDrive> {
+export class AudioManager<T extends IAudioDrive> implements IAudioDrive {
     private _drive: T
     constructor(drive: T) {
         this._drive = drive
     }
     getDrive() {
         return this._drive
+    }
+    playEffect(url: string) {
+        this._drive.playEffect(url)
+    }
+    playMusic(url: string) {
+        this._drive.playMusic(url)
+    }
+    stopMusic() {
+        this._drive.stopMusic()
+    }
+    resumeAll() {
+        this._drive.resumeAll()
+    }
+    pauseAll() {
+        this._drive.pauseAll()
+    }
+    getMusicVolume() {
+        return this._drive.getMusicVolume()
+    }
+    setMusicVolume(val: number) {
+        this._drive.setMusicVolume(val)
+    }
+    getEffectVolume() {
+        return this._drive.getEffectVolume()
+    }
+    setEffectVolume(val: number) {
+        this._drive.setEffectVolume(val)
     }
 }
