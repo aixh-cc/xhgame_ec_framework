8f5f2fcf9c7c91603a438f335ae2d7f89d6af359
hottredpen <277488199@qq.com>
2025-10-16 13:26:26 +0800

    修复再次修改内容的问题

--- packages/core/src/Ui/ViewUtil.ts
+++ packages/core/src/Ui/ViewUtil.ts
@@ -15,18 +15,7 @@ export class ViewUtil {
                 if (!modelComp) {
                     continue;
                 }
-                let vals_arr: string[] = _bindAttrLink.replace(compName + '::', '').split('.');
-                // 逐层检查属性
-                const [valid, last_i, pre_current] = this._getMoreInfo(modelComp, vals_arr)
-                if (valid) {
-                    // let final_path = this._getFinalPrePath(compName, vals_arr)
-                    // if (last_i == vals_arr.length - 1) {
-                    //     DI.bindInstance(final_path, pre_current)
-                    // } else {
-                    //     // notify时重新查找
-                    // }
-                    modelComp.attachObserver(observer)
-                }
+                modelComp.attachObserver(observer)
             }
         }
     }
@@ -42,27 +31,19 @@ export class ViewUtil {
                 continue; // 跳过
             }
             let vals_arr: string[] = _bindAttrLink.replace(modelComp.compName + '::', '').split('.');
-            let last_key = key
-            if (vals_arr.length > 0) {
-                last_key = vals_arr[vals_arr.length - 1]
-            }
-            let final_path = this._getFinalPrePath(modelComp.compName, vals_arr)
-            let vvv = DI.safeMake(final_path) as any
-            // if (!vvv) {
-            //     console.log('未找到final_path=', final_path)
-            //     // 再次逐层检查属性
+            let vvv = null
             const [valid, last_i, pre_current] = this._getMoreInfo(modelComp, vals_arr)
             if (valid) {
-                let final_path = this._getFinalPrePath(modelComp.compName, vals_arr)
                 if (last_i == vals_arr.length - 1) {
-                    // console.log(final_path + '重新注入成功', pre_current)
-                    DI.bindInstance(final_path, pre_current)
                     vvv = pre_current
                 }
             }
-            // }
             if (vvv) {
                 const that = observer as any
+                let last_key = key
+                if (vals_arr.length > 0) {
+                    last_key = vals_arr[vals_arr.length - 1]
+                }
                 if (typeof that[key] != 'undefined' && vvv[last_key] != 'undefined') {
                     that[key] = vvv[last_key];
                 }
@@ -70,21 +51,6 @@ export class ViewUtil {
                 continue;// 跳过
             }
         }
-    }
-    static _getFinalPrePath(compName: string, vals_arr: any[]) {
-        let final_pre_object_attr_map = ''
-        if (vals_arr.length <= 1) {
-            final_pre_object_attr_map = compName // 只有一个的话默认取compName
-        } else {
-            let _arrs = []
-            for (let i = 0; i < vals_arr.length; i++) {
-                if (i < vals_arr.length - 1) {
-                    _arrs.push(vals_arr[i])
-                }
-            }
-            final_pre_object_attr_map = compName + '::' + _arrs.join('.')
-        }
-        return final_pre_object_attr_map
     }
     static _getMoreInfo(modelComp: BaseModelComp, vals_arr: string[]) {
         let current = modelComp as any;
