2811d588c5295c7cd64260b63fbcc955f281e972
hottredpen <277488199@qq.com>
2025-10-18 08:05:51 +0800

    移动3dDemo到xhgame_kits中

--- 3dDemo/assets/script/Globle.d.ts
+++
@@ -1,5 +0,0 @@
-// 定义微信全局变量ws
-declare const wx;
-
-// 抖音
-declare const tt;
--- 3dDemo/assets/script/managers/myTable/MyTableConfig.ts
+++
@@ -1,8 +0,0 @@
-import { ISkillTableItem, SkillTable } from "./tables/SkillTable";
-import { IUnitTableItem, UnitTable } from "./tables/UnitTable";
-import { BattleTable, IBattleTableItem } from "./tables/BattleTable";
-import { IStoreTableItem, StoreTable } from "./tables/StoreTable";
-import { ITableConfig } from "@aixh-cc/xhgame_ec_framework";
-import { ConfigTable, IConfigTableItem } from "./tables/ConfigTable";
-import { TableType } from "../MyTableManager";
-
