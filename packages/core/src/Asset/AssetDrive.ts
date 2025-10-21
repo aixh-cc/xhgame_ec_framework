5cef14bd03cd2e755b7a9c32da362edf7f379578
hottredpen <277488199@qq.com>
2025-10-21 12:59:34 +0800

    asset

--- packages/core/src/Asset/AssetDrive.ts
+++ packages/core/src/Asset/AssetDrive.ts
@@ -1,5 +1,6 @@
 export interface IBundle {
     loadDir(dir: string, onProgress: ((finished: number, total: number, item: any) => void) | null, onComplete: (err: Error | null, data: any[]) => void): void
+    load<T>(paths: string, type: T | null, onComplete?: ((err: Error | null, data: T) => void) | null): void;
 }
 
 export interface IAssetDrive {
