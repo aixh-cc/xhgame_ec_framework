86e286d6e150dd6522207326adb537c30e0a8f8c
hottredpen <277488199@qq.com>
2025-11-06 12:24:10 +0800

    打包测试

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -3,3 +3,5 @@ export * from './Builder/Defined';
 export * from './Builder/Handles';
 export * from './Builder/InstallInfoManager';
 export * from './Builder/Util';
+// 打包方法
+export * from './Pack/Pack';
