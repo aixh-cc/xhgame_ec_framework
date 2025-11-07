2c527b58f5e02ac327e8486a9a1aae1386c3ff39
hottredpen <277488199@qq.com>
2025-11-07 12:36:50 +0800

    添加注释

--- packages/builder/src/Builder/Util.ts
+++ packages/builder/src/Builder/Util.ts
@@ -1,6 +1,10 @@
 import { join, resolve } from 'path';
 import * as fs from 'fs';
 
+/**
+ * 获取 Cocos Creator 版本号
+ * @returns Cocos Creator 版本号
+ */
 export async function getCocosVersion(): Promise<string> {
     const projectPath = getProjectPath();
     const packagePath = join(projectPath, 'package.json');
@@ -14,7 +18,11 @@ export async function getCocosVersion(): Promise<string> {
     return 'unknown'
 }
 
-export const getProjectPath = () => {
+/**
+ * 获取项目路径
+ * @returns 项目路径
+ */
+export const getProjectPath = (): string => {
     if (typeof Editor != 'undefined' && Editor.Project) {
         return Editor.Project.path;
     } else {
@@ -44,24 +52,44 @@ export const getProjectPath = () => {
     }
 };
 
+/**
+ * 获取 extensions 目录路径
+ * @returns extensions 目录路径
+ */
 export const getExtensionsPath = () => {
     let projectPath = getProjectPath()
     return join(projectPath, 'extensions');
 };
 
+/**
+ * 获取插件目录路径
+ * @param pluginName 插件名称
+ * @returns 插件目录路径
+ */
 export const getPluginPath = (pluginName: string) => {
     let projectPath = getProjectPath()
     return join(projectPath, 'extensions', pluginName);
 };
 
+/**
+ * 获取插件的 packages 目录路径
+ * @param pluginName 插件名称
+ * @returns packages 目录路径
+ */
 export const getPackagesPath = (pluginName: string) => {
     let projectPath = getProjectPath()
     return join(projectPath, 'extensions', pluginName, 'packages');
 };
 
-export const getGroupPath = (pluginName: string, target: string) => {
+/**
+ * 获取插件的目标目录路径
+ * @param pluginName 插件名称
+ * @param group 目标目录名称
+ * @returns 目标目录路径
+ */
+export const getGroupPath = (pluginName: string, group: string) => {
     let projectPath = getProjectPath()
-    return join(projectPath, 'extensions', pluginName, 'packages', target);
+    return join(projectPath, 'extensions', pluginName, 'packages', group);
 };
 
 
